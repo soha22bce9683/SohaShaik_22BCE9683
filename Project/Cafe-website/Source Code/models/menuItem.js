c53d7f321f128ce6a77ce6af8a1c2c9dcbc5f878
soha22bce9683 <soha.22bce9683@vitapstudent.ac.in>
2025-06-15 15:30:58 +0530

    Delete Project/Cafe-website/Source Code/models directory

--- Project/Cafe-website/Source Code/models/menuItem.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const menuItemSchema = new mongoose.Schema({
-  itemName: String,
-  price: Number
-});
-
-module.exports = mongoose.model('MenuItem', menuItemSchema);
